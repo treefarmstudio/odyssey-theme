aefb8c786c36c8b7d6c25d56bc175c4e685fd369
Jaydan Urwin <jaydan@jaydanurwin.com>
2022-05-15 11:21:32 -0600

    added sitemap plugin

--- theme/astro.config.mjs
+++ theme/astro.config.mjs
@@ -1,8 +1,13 @@
 import { defineConfig } from 'astro/config';
 import lit from '@astrojs/lit';
+import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://odyssey-theme.littlesticks.dev/', // Your public domain, e.g.: https://my-site.dev/. Used to generate sitemaps and canonical URLs.
-	sitemap: true, // Generate sitemap (set to "false" to disable)
-	integrations: [lit()], // Add renderers to the config
+  site: 'https://odyssey-theme.littlesticks.dev/',
+  // Your public domain, e.g.: https://my-site.dev/. Used to generate sitemaps and canonical URLs.
+  sitemap: true,
+  // Generate sitemap (set to "false" to disable)
+  integrations: [lit(), sitemap()] // Add renderers to the config
+
 });
