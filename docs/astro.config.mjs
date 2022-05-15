e71eb90657dce918e2e4d091c565fbb173afecbb
Jaydan Urwin <jaydan@jaydanurwin.com>
2022-05-15 11:23:07 -0600

    added sitemap to docs

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -2,12 +2,16 @@ import { defineConfig } from 'astro/config';
 import preact from '@astrojs/preact';
 import react from '@astrojs/react';
 
+import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-		// Enable Preact to support Preact JSX components.
-		preact(),
-		// Enable React for the Algolia search component.
-		react(),
-	],
+  site: 'https://odyssey-theme-docs.littlesticks.dev/',
+  integrations: [
+  // Enable Preact to support Preact JSX components.
+  preact(), 
+  // Enable React for the Algolia search component.
+  react(), 
+  sitemap(),
+]
 });
