83be79298cc2939a881292d310089415b6076a75
Jaydan Urwin <jaydan@jaydanurwin.com>
2022-08-10 23:08:28 -0600

    removed unused lit renderer

--- theme/astro.config.mjs
+++ theme/astro.config.mjs
@@ -1,5 +1,4 @@
 import { defineConfig } from 'astro/config';
-import lit from '@astrojs/lit';
 import sitemap from "@astrojs/sitemap";
 
 import mdx from "@astrojs/mdx";
@@ -10,6 +9,6 @@ export default defineConfig({
   // Your public domain, e.g.: https://my-site.dev/. Used to generate sitemaps and canonical URLs.
   sitemap: true,
   // Generate sitemap (set to "false" to disable)
-  integrations: [lit(), sitemap(), mdx()] // Add renderers to the config
+  integrations: [sitemap(), mdx()] // Add renderers to the config
 
 });
