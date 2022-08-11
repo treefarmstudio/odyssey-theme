91ea97fe979d35bb31627795070f8418f75ed316
Jaydan Urwin <jaydan@jaydanurwin.com>
2022-08-10 22:56:09 -0600

    moved to mdx posts

--- theme/astro.config.mjs
+++ theme/astro.config.mjs
@@ -2,12 +2,14 @@ import { defineConfig } from 'astro/config';
 import lit from '@astrojs/lit';
 import sitemap from "@astrojs/sitemap";
 
+import mdx from "@astrojs/mdx";
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://odyssey-theme.littlesticks.dev/',
   // Your public domain, e.g.: https://my-site.dev/. Used to generate sitemaps and canonical URLs.
   sitemap: true,
   // Generate sitemap (set to "false" to disable)
-  integrations: [lit(), sitemap()] // Add renderers to the config
+  integrations: [lit(), sitemap(), mdx()] // Add renderers to the config
 
 });
