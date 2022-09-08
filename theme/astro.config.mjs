9c1bf66c63cc79af5f3aa9aae4130d638fed9b39
Jaydan Urwin <jaydan@jaydanurwin.com>
2022-09-07 23:02:28 -0600

    setting up astro image integration

--- theme/astro.config.mjs
+++ theme/astro.config.mjs
@@ -1,7 +1,8 @@
 import { defineConfig } from 'astro/config';
 import sitemap from "@astrojs/sitemap";
-
 import mdx from "@astrojs/mdx";
+
+import image from "@astrojs/image";
 
 // https://astro.build/config
 export default defineConfig({
@@ -9,6 +10,6 @@ export default defineConfig({
   // Your public domain, e.g.: https://my-site.dev/. Used to generate sitemaps and canonical URLs.
   sitemap: true,
   // Generate sitemap (set to "false" to disable)
-  integrations: [sitemap(), mdx()] // Add renderers to the config
+  integrations: [sitemap(), mdx(), image()] // Add renderers to the config
 
 });
