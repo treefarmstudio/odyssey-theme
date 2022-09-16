93ed00b8241c4da4f18f8581cfa057e6835bfcc2
Jaydan Urwin <jaydan@jaydanurwin.com>
2022-09-16 00:23:59 -0600

    show ls plug is optional

--- theme/src/config/settings.js
+++ theme/src/config/settings.js
@@ -4,4 +4,5 @@ export default {
 	url: `https://odyssey-theme.littlesticks.dev`, // No trailing slash!
 	name: `Odyssey`, // The short name of the business or brand name. Used for things like the copyright in the footer.
   enableThemeSwitcher: true,
+  showLittleSticksPlug: true, // Disable this if you want to remove the Little Sticks plug from the footer. (╯°□°)╯︵ ┻━┻
 };
