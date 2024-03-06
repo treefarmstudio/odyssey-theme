a272c530f83006469444ce2973ca322209048edb
Jaydan Urwin <jaydan@whitepine.studio>
2024-03-06 11:18:54 -0700

    updating urls

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import lit from "@astrojs/lit";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://odyssey-theme.littlesticks.dev/',
+  site: 'https://odyssey-theme.sapling.supply/',
   // Your public domain, e.g.: https://my-site.dev/. Used to generate sitemaps and canonical URLs.
   sitemap: true,
   // Generate sitemap (set to "false" to disable)
